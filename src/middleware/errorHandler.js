b06993f27c8d032c4a806d814ebbeed046d57b48
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-01 21:20:28 +0200

    deployment

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,7 +2,7 @@
 function errorHandlerMiddleware (err, req, res, next) {
     console.log(err);
     if(!err.status) { //other error
-        return res.status(500).send({"error": "internal server error"});
+        return res.status(500).send({"error": err.messege});
     }
      return res.status(err.status).send({"error": err.messege}) ; 
 }
